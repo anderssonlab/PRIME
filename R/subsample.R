68e2c26828cd1e7404e2ece2ab124a3369033895
anderssonrobin <andersson.robin@gmail.com>
2024-06-10 09:50:05 +0200

    update

--- R/subsample.R
+++ R/subsample.R
@@ -1,51 +1,69 @@
-require("CAGEfightR")
-require("assertthat")
-
-source("CAGEfightR_extensions/utils.R")
-
-## Subsample a SummarizedExperiment to a target sequencing depth
+#' Subsample a \code{SummarizedExperiment} to a target sequencing depth.
+#' 
+#' @param object A \code{SummarizedExperiment} object.
+#' @param inputAssay The assay to use.
+#' @param target The target sequencing depth.
+#' 
+#' @return A \code{SummarizedExperiment} object with the subsampled counts.
+#' 
+#' @export
 subsampleTarget <- function(object, inputAssay = "counts", target) {
-
-    assert_that(methods::is(object, "SummarizedExperiment"),
-                inputAssay %in% assayNames(object),
-                is.numeric(target), target > 0)
-    
-    a <- assay(object,inputAssay)
-    n <- ncol(a)
-    nz <- lapply(1:n, function(i) nonzero(a[,i,drop=FALSE])[,1])
-    s <- Matrix::colSums(a)
-    d <- unlist(lapply(1:n, function(i) {
-        if (s[i]<=target)
-            a[nz[[i]],i]
-        else
-            rbinom(length(nz[[i]]),a[nz[[i]],i], target/s[i])
-    }))
-    keep <- which(sapply(nz,length)>0)
-    assay(object, inputAssay) <- Matrix::sparseMatrix(i=unlist(nz),
-                                                      j=unlist(lapply(keep, function(i) rep(i,length(nz[[i]])))),
-                                                      x=d, dimnames=list(rownames(a), colnames(a)[keep]))
-    
-    object
+  
+  assertthat::assert_that(methods::is(object, "SummarizedExperiment"),
+                          inputAssay %in% assayNames(object),
+                          is.numeric(target), target > 0)
+  
+  a <- assay(object,inputAssay)
+  n <- ncol(a)
+  nz <- lapply(1:n, function(i) nonzero(a[,i,drop=FALSE])[,1])
+  s <- Matrix::colSums(a)
+  d <- unlist(lapply(1:n, function(i) {
+    if (s[i]<=target)
+      a[nz[[i]],i]
+    else
+      rbinom(length(nz[[i]]),a[nz[[i]],i], target/s[i])
+  }))
+  keep <- which(sapply(nz,length)>0)
+  assay(object, inputAssay) <- 
+    Matrix::sparseMatrix(i=unlist(nz),
+                         j=unlist(lapply(keep, function(i) {
+                           rep(i,length(nz[[i]]))})),
+                         x=d, dimnames=list(rownames(a), 
+                                            colnames(a)[keep]))
+  
+  object
 }
 
-## Subsample a SummarizedExperiment to a proportion of its sequencing depth
-## Useful for saturation analyses
+#' Subsample a \code{SummarizedExperiment} to a proportion of its sequencing 
+#' depth. Useful for saturation analyses.
+#' 
+#' @param object A \code{SummarizedExperiment} object.
+#' @param inputAssay The assay to use.
+#' @param proportion The proportion of the sequencing depth to subsample.
+#' 
+#' @return A \code{SummarizedExperiment} object with the subsampled counts.
+#' 
+#' @export
 subsampleProportion <- function(object, inputAssay = "counts", proportion) {
- 
-    assert_that(methods::is(object, "SummarizedExperiment"),
-                inputAssay %in% assayNames(object),
-                is.numeric(proportion), proportion > 0 && proportion <=1)
-    
-    a <- assay(object,inputAssay)
-    n <- ncol(a)
-    nz <- lapply(1:n, function(i) nonzero(a[,i,drop=FALSE])[,1])
-    d <- unlist(lapply(1:n, function(i) {
-        rbinom(length(nz[[i]]),a[nz[[i]],i], proportion)
-    }))
-    keep <- which(sapply(nz,length)>0)
-    assay(object, inputAssay) <- Matrix::sparseMatrix(i=unlist(nz),
-                                                      j=unlist(lapply(keep, function(i) rep(i,length(nz[[i]])))),
-                                                      x=d, dimnames=list(rownames(a), colnames(a)[keep]))
-    
-    object
+  
+  assertthat::assert_that(methods::is(object, "SummarizedExperiment"),
+                          inputAssay %in% assayNames(object),
+                          is.numeric(proportion), 
+                          proportion > 0 && proportion <=1)
+  
+  a <- assay(object,inputAssay)
+  n <- ncol(a)
+  nz <- lapply(1:n, function(i) nonzero(a[,i,drop=FALSE])[,1])
+  d <- unlist(lapply(1:n, function(i) {
+    rbinom(length(nz[[i]]),a[nz[[i]],i], proportion)
+  }))
+  keep <- which(sapply(nz,length)>0)
+  assay(object, inputAssay) <- 
+    Matrix::sparseMatrix(i=unlist(nz),
+                         j=unlist(lapply(keep, function(i) {
+                           rep(i,length(nz[[i]]))})),
+                         x=d, dimnames=list(rownames(a), 
+                                            colnames(a)[keep]))
+  
+  object
 }
--- R/support.R
+++ R/support.R
@@ -1,27 +1,40 @@
-require("CAGEfightR")
-require("assertthat")
-
-source("CAGEfightR_extensions/utils.R")
-
-## object: RangedSummarizedExperiment
-## categorical: factor
-
-calcBatchSupport <- function(object, batch, inputAssay="counts", outputColumn="batchSupport", unexpressed=0, minSamples=1) {
-    
-    ## dimensions
-    assert_that(ncol(object)==length(batch),
-                length(unique(batch))>1,
-                minSamples>=1)
-
-    if (outputColumn %in% colnames(rowData(object))) {
-        warning("object already has a column named ", outputColumn, " in rowData: It will be overwritten!")
-    }
-
-    message("Calculating support per batch...")
-    a <- do.call("cbind", lapply(unique(batch), function(b) Matrix::rowSums(assay(object,inputAssay)[,batch==b,drop=FALSE]>unexpressed)))
-
-    message("Calculating overall batch support...")
-    rowData(object)[, outputColumn] <- as.integer(Matrix::rowSums(a>=minSamples))
-
-    object
+#' Calculate batch support for expression data.
+#' 
+#' @param object A \code{SummarizedExperiment} object.
+#' @param batch A \code{factor} indicating the batch of each sample.
+#' @param inputAssay The assay to use.
+#' @param outputColumn The name of the column to store the batch support.
+#' @param unexpressed The threshold below which a unit (CTSS, gene, etc.) 
+#' is considered unexpressed.
+#' @param minSamples The minimum number of samples in which a unit must be
+#' expressed to be considered supported.
+#' 
+#' @return A \code{SummarizedExperiment} object with the batch support column.
+#' 
+#' @export
+calcBatchSupport <- function(object, batch, inputAssay="counts", 
+                             outputColumn="batchSupport", unexpressed=0, 
+                             minSamples=1) {
+  
+  ## dimensions
+  assert_that(ncol(object)==length(batch),
+              length(unique(batch))>1,
+              minSamples>=1)
+  
+  if (outputColumn %in% colnames(rowData(object))) {
+    warning("object already has a column named ", outputColumn, 
+            " in rowData: It will be overwritten!")
+  }
+  
+  message("Calculating support per batch...")
+  a <- 
+    do.call("cbind", lapply(unique(batch), function(b) {
+      Matrix::rowSums(assay(object,inputAssay)[,batch==b,drop=FALSE]>
+                        unexpressed)
+    }))
+  
+  message("Calculating overall batch support...")
+  rowData(object)[, outputColumn] <- as.integer(Matrix::rowSums(a>=minSamples))
+  
+  object
 }
