ed1409a7704b5104de40ea0f068dcd24702f0772
Robin Andersson <andersson.robin@gmail.com>
2026-06-08 11:52:04 +0200

    script to recreate ext daa

---
+++ data-raw/make-ctss-extdata.R
@@ -0,0 +1,64 @@
+# Regenerate packaged CTSS example objects for vignettes.
+# Run from the package root with:
+#   Rscript data-raw/make-ctss-extdata.R
+
+suppressPackageStartupMessages({
+  library(CAGEfightR)
+  library(GenomeInfoDb)
+  library(PRIME)
+  library(rtracklayer)
+  library(SummarizedExperiment)
+})
+
+package_root <- normalizePath(file.path(dirname(sys.frame(1)$ofile %||% "data-raw/make-ctss-extdata.R"), ".."), mustWork = FALSE)
+if (!file.exists(file.path(package_root, "DESCRIPTION"))) {
+  package_root <- normalizePath(getwd(), mustWork = TRUE)
+}
+
+design_file <- file.path(package_root, "inst", "extdata", "design_matrix_first10pct.tsv")
+bw_dir <- file.path(package_root, "inst", "extdata", "cage_bw")
+out_ctss <- file.path(package_root, "inst", "extdata", "ctss.rds")
+out_ctss_clean <- file.path(package_root, "inst", "extdata", "ctss_clean.rds")
+
+if (!file.exists(design_file)) {
+  stop("Missing design matrix: ", design_file)
+}
+if (!dir.exists(bw_dir)) {
+  stop("Missing BigWig directory: ", bw_dir)
+}
+
+message("Reading design matrix: ", design_file)
+design <- read.table(design_file, header = TRUE, sep = "\t")
+rownames(design) <- design$Name
+
+bw_plus <- rtracklayer::BigWigFileList(file.path(bw_dir, design$BigWigPlus))
+bw_minus <- rtracklayer::BigWigFileList(file.path(bw_dir, design$BigWigMinus))
+names(bw_plus) <- names(bw_minus) <- design$Name
+
+message("Quantifying CTSSs")
+ctss <- CAGEfightR::quantifyCTSSs(
+  plusStrand = bw_plus,
+  minusStrand = bw_minus,
+  design = design
+)
+ctss <- CAGEfightR::calcTotalTags(ctss)
+ctss <- CAGEfightR::calcTPM(ctss)
+ctss <- CAGEfightR::calcPooled(ctss)
+ctss <- GenomeInfoDb::keepStandardChromosomes(ctss, pruning.mode = "coarse")
+
+message("Creating singleton-filtered CTSS object")
+min_depth <- min(SummarizedExperiment::colData(ctss)$totalTags)
+ctss_sub <- PRIME::subsampleTarget(ctss, target = min_depth)
+ctss_sub <- CAGEfightR::calcTPM(ctss_sub)
+ctss_clean <- PRIME::rmSingletons(ctss_sub)
+
+message("Writing: ", out_ctss)
+saveRDS(ctss, out_ctss, compress = "xz")
+message("Writing: ", out_ctss_clean)
+saveRDS(ctss_clean, out_ctss_clean, compress = "xz")
+
+message("Done")
+
+`%||%` <- function(x, y) {
+  if (is.null(x)) y else x
+}
